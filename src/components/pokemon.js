cf2634890e0aac36e9c32df97c629027ccbab8fa
Marc <marc@haupt.digital>
2020-03-02 16:13:54 +0100

    Webpack (#2)
    * Basic setup
    
    * Add webpack config and scripts
    
    * Install HtmlWebpackPlugin and configuration
    
    * Install CleanWebpackPlugin and configure
    
    * Install webpack dev server and configuration
    
    * Install Sass dependecies
    
    style-loader, css-loader, sass-loader
    
    * Update eslint test script

---
+++ src/components/pokemon.js
@@ -0,0 +1,7 @@
+function pokemon() {
+  const element = document.createElement('div');
+  element.innerText = 'Shiggy';
+  return element;
+}
+
+export default pokemon;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+     new CleanWebpackPlugin(),
+     new HtmlWebpackPlugin({
+       title: 'Pokedex Example',
+     }),
+   ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+     contentBase: './dist',
+   },
+   module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+};
