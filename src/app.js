2f66171e0f6bde9aa13742b9bfc5f547b0891be4
hauptdigital <marc@haupt.digital>
2020-03-03 12:05:06 +0100

    Add logo and font css

--- src/app.js
+++ src/app.js
@@ -10,9 +10,14 @@ export function app() {
   const main = createElement('main', {
     className: 'main'
   });
+  const logo = createElement('img', {
+    className: 'header__logo',
+    src: 'https://img.icons8.com/color/96/000000/pokeball--v1.png'
+  });
   const titleElement = title('Pokedex');
-  const searchElement = search('Hier nach Pokémon suchen');
+  const searchElement = search('Pokémon suchen...');
 
+  header.appendChild(logo);
   header.appendChild(titleElement);
   main.appendChild(searchElement);
 
